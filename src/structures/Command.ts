e1595f14506c7953e95136746ec9b68715d2021a
langtieu369 <karlukwilson@gmail.com>
2026-09-14 02:01:13 +0700

    Update Command.ts

--- src/structures/Command.ts
+++ src/structures/Command.ts
@@ -1,2 +1,22 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';import { TuTienClient } from '../client/TuTienClient';
-export abstract class Command{constructor(public readonly data:SlashCommandBuilder){} abstract execute(client:TuTienClient,i:ChatInputCommandInteraction):Promise<unknown>|unknown;}
+import {
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  SlashCommandOptionsOnlyBuilder,
+  SlashCommandSubcommandsOnlyBuilder
+} from 'discord.js';
+
+import { TuTienClient } from '../client/TuTienClient';
+
+export type CommandBuilder =
+  | SlashCommandBuilder
+  | SlashCommandOptionsOnlyBuilder
+  | SlashCommandSubcommandsOnlyBuilder;
+
+export abstract class Command {
+  constructor(public readonly data: CommandBuilder) {}
+
+  abstract execute(
+    client: TuTienClient,
+    interaction: ChatInputCommandInteraction
+  ): Promise<unknown> | unknown;
+}
